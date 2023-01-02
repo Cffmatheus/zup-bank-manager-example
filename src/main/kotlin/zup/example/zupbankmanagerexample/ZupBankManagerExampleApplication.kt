ec423bf21c4eb620b5ccfc6c9c378e12c6d6f8a8
Matheus Candido <matheus.faria@zup.com.br>
2023-01-02 16:46:50 -0300

    build: project started

---
+++ src/main/kotlin/zup/example/zupbankmanagerexample/ZupBankManagerExampleApplication.kt
@@ -0,0 +1,11 @@
+package zup.example.zupbankmanagerexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZupBankManagerExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZupBankManagerExampleApplication>(*args)
+}
---
+++ src/test/kotlin/zup/example/zupbankmanagerexample/ZupBankManagerExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package zup.example.zupbankmanagerexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZupBankManagerExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
